069a6c315e757dad52d4ddcb53ead4f178ce27a6
Frederico S Almeida <almeidaa.frederico@gmail.com>
2023-04-06 22:43:11 -0300

    feat: detail

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Frederico Almeida",
+  githubUser: "almeidafrederico",
+  linkedinUser: "almeidaafrederico",
+  whatsappNumber: "+5511953863459",
+  emailUser: "almeidaa.frederico@gmail.com",
+  instagramUser: "fs.dealmeida",
   facebookUser: "",
 };
